679a3f601917c5a571552f16d8a92ef21138820c
Nicolas Gagne <ngagne966@gmail.com>
2017-06-10 21:16:06 -0400

    update
    implement
    - multigame
    - color text
    - etc

--- Battleship 2.py
+++ Battleship 2.py
@@ -1,32 +1,13 @@
 
 import colorama
+from colorama import init
+init()
+
 from colorama import Fore, Back, Style
 from random import randint
-board = []
-ships = []
-ready = False
-while ready == False:
-	print
-	board_size = int(input('What size do you want your board??'))
-	n_try = int(input('How much try do you want??'))
-	n_ships = int(input('How much ship do you want??'))	
-	
-	if board_size >= 5 and n_ships <= board_size:
-		ready = True
-	if board_size < 5:
-		print ('Board too Small!')
-	if n_ships > board_size:
-		print ('Too much ship for the Board size!')
-	
 
-for x in range(board_size):
-	board.append(["O"] * board_size)
 
-def print_board(board):
-	for row in board:
-		print (" ".join(row))
 
-    
 
 def random_row(board):
 	return randint(0, len(board) - 1)
@@ -75,8 +56,12 @@ def game_over(board, ships):
 		for point in ship:
 			board[point[0]][point[1]] = 'S'
 	print_board(board)
-	print
-	print ('GAME OVER')
+	print()
+	print('GAME OVER')
+	print()
+	return input('Do you want to play again? (y/n) ')
+	
+	
 	
 def detail_ship(ships):
 	print ('Ship detail')
@@ -86,95 +71,163 @@ def detail_ship(ships):
 		sh += 1
 	print
 	
+def print_board(board):
+	for row in board:
+		print (" ".join(row))
+
+def new_game():
+	ready = False
+	while ready == False:
+		board_size = input('What size do you want your board??')
+		try :
+			board_size = int(board_size)					
+			if board_size < 5:
+				print ('Board too Small!')
+			else:
+				ready = True	
+		except ValueError:
+			print('Enter a integral number')
 	
 
-	
-## Create all the Ship.
-
-
-ships.append(ship_built(randint(2, 5), board_size - 1))	
-print ('Ship', ships)
-
-for x in range(n_ships - 1):
-	ship_separate = False
-	while ship_separate == False:
-		ship_separate = True
-		ship_test = ship_built(randint(2, 5), board_size - 1)
-		for ship in ships:
-			for pts in ship :
-				for t in ship_test:
-					if pts == t:
-						ship_separate = False
-						#print('New ship overlap other one!')
-	print('Ship', ship_test)
-	ships.append(ship_test)
-
-#print ('SHIPS', ships)
-detail_ship(ships)
-
-
-print ("Let's play Battleship!")
-ship
-
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
-for turn in range(n_try):
-	
-	hit_list = []
-	hit = True
-	print
-	print ('It is turn:', turn + 1, 'of ', n_try)
-	while hit == True:
-		print_board(board)
-		hit = False
-		row = int(input("Guess Row: "))- 1
-		col = int(input("Guess Col: "))- 1
-		guess = [row, col]
+	ready = False
+	while ready == False:		
+		n_ships = input('How much ship do you want??')		
+		try:
+			n_ships = int(n_ships)
+			if n_ships <= board_size:
+				ready = True
+			else:
+				print ('Too much ship for the Board size!')	
+		except ValueError:
+			print('Enter a integral number')
 		
-		if guess[0] < 0 or guess[0] > board_size - 1 or guess[1] < 0 or guess[1] > board_size - 1:
-			print ("Oops, that's not even in the ocean.")
-		elif board[guess[0]][guess[1]] == "X" or board[guess[0]][guess[1]] == "H" :
-			print ("You guessed that one already.")
-		else:
+	ready = False
+	while ready == False:	
+		n_try = input('How much try do you want??')
+		try:
+			n_try = int(n_try)
+			ready = True
+		except ValueError:
+			print('Enter a integral number')
+			
+	return board_size, n_ships, n_try
+
+def create_board(board_size):
+	for x in range(board_size):
+		board.append(["O"] * board_size)	
+	
+	
+def create_Ship():
+	## Create all the Ship.
+	ships.append(ship_built(randint(2, 5), board_size - 1))	
+	print ('Ship', ships)
+
+	for x in range(n_ships - 1):
+		ship_separate = False
+		while ship_separate == False:
+			ship_separate = True
+			ship_test = ship_built(randint(2, 5), board_size - 1)
 			for ship in ships:
-				if board[guess[0]][guess[1]] == 'H':
-					break
-				for pts in ship:
-					if pts == guess:
-						hit = True
-						print(Fore.RED +'You hit a battleship')
-						board[guess[0]][guess[1]] = 'H'
-						hit_list.append(guess)
-						break
-					else:
-						board[guess[0]][guess[1]] = 'X'
-			if board[guess[0]][guess[1]] == 'X':
-				print ("You missed the battleship!")
-				
-		
-		if hit_list != []:
-		
-			for ht in hit_list:
-				for ship in ships:
-					if ht in ship:
-						ship.remove(ht)
-			des_ship = []
-			for s in range(len(ships)):
-				if ships[s] == []:
-					print ("Congratulations! You sunk one battleship!")
-					des_ship.append(s)
-			for ds in des_ship:
-				ships.remove(ships[ds])
-	
-		if ships == []:
-			hit = False
-			print
-			print  ("Congratulations! You sunk all the Fleet. ")
-			print
-			break
-		
+				for pts in ship :
+					for t in ship_test:
+						if pts == t:
+							ship_separate = False
+							#print('New ship overlap other one!')
+		print('Ship', ship_test)
+		ships.append(ship_test)
 
-game_over(board, ships)	
+def play():
+	print()
+	print (Back.GREEN + "Let's play Battleship!")
+	print(Style.RESET_ALL)
+
+
+	for turn in range(n_try):
+		
+		if ships != []:	
+			hit_list = []
+			hit = True
+			print
+			print ('It is turn:', turn + 1, 'of ', n_try)
+			while hit == True:
+				print_board(board)
+				hit = False
+				
+				try:
+					row = int(input("Guess Row: "))- 1
+					col = int(input("Guess Col: "))- 1
+				except ValueError:
+					row = board_size + 5
+					col = board_size + 5
+								
+				guess = [row, col]
+				
+				if guess[0] < 0 or guess[0] > board_size - 1 or guess[1] < 0 or guess[1] > board_size - 1:
+					print ("Oops, that's not even in the ocean.")
+				elif board[guess[0]][guess[1]] == "X" or board[guess[0]][guess[1]] == "H" :
+					print (Fore.YELLOW + "You guessed that one already.")
+					print(Style.RESET_ALL)
+				else:
+					for ship in ships:
+						if board[guess[0]][guess[1]] == 'H':
+							break
+						for pts in ship:
+							if pts == guess:
+								hit = True
+								print(Fore.RED +'You hit a battleship')
+								print(Style.RESET_ALL)
+								board[guess[0]][guess[1]] = 'H'
+								hit_list.append(guess)
+								break
+							else:
+								board[guess[0]][guess[1]] = 'X'
+					if board[guess[0]][guess[1]] == 'X':
+						print ("You missed the battleship!")
+						
+				
+				if hit_list != []:
+				
+					for ht in hit_list:
+						for ship in ships:
+							if ht in ship:
+								ship.remove(ht)
+					des_ship = []
+					for s in range(len(ships)):
+						if ships[s] == []:
+							print (Fore.RED + "Congratulations! You sunk one battleship!")
+							print(Style.RESET_ALL)
+							des_ship.append(s)
+					for ds in des_ship:
+						ships.remove(ships[ds])
+			
+				if ships == []:
+					hit = False
+					print
+					print(Fore.RED +"Congratulations! You sunk all the Fleet. ")
+					print(Style.RESET_ALL)
+					print
+					break
+			
+ng = 'y'	
+while ng == 'y':
+	board = []
+	ships = []
+	board_size, n_ships, n_try = new_game()
+	create_board(board_size)
+	create_Ship()
+	detail_ship(ships) #TESTING Perpesus
+	play()
+	if game_over(board, ships)	!= 'y':
+		ng = 'n'
+		print()
+		print()
+	
+
+
+	
+
+
+
 	
 #if guess_row == ship_row and guess_col == ship_col:
 #    print ("Congratulations! You sunk one battleship!")
